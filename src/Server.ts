8ed74d3111e46cc9743b2912f9d20f861b3be941
romain.lenzotti <rom.lenzotti@gmail.com>
2020-02-02 12:38:19 +0100

    'Deploy project with Ts.ED v5.41.0'

--- src/Server.ts
+++ src/Server.ts
@@ -1,34 +1,60 @@
-import {GlobalAcceptMimesMiddleware, ServerLoader, ServerSettings} from "@tsed/common";
+import {
+  ConverterService,
+  EndpointInfo,
+  GlobalAcceptMimesMiddleware,
+  IMiddleware,
+  OverrideProvider,
+  Res,
+  ResponseData,
+  SendResponseMiddleware,
+  ServerLoader,
+  ServerSettings
+} from "@tsed/common";
+import {isBoolean, isNumber, isStream, isString} from "@tsed/core";
+import "@tsed/passport";
 import "@tsed/swagger";
 import "@tsed/typeorm";
+import * as bodyParser from "body-parser";
+import * as compress from "compression";
+import * as cookieParser from "cookie-parser";
+import * as cors from "cors";
+import * as session from "express-session";
+import * as methodOverride from "method-override";
+import {User} from "./entities/User";
 
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const compress = require("compression");
-const methodOverride = require("method-override");
-const session = require("express-session");
 const rootDir = __dirname;
 
 @ServerSettings({
   rootDir,
-  port: 3000,
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false,
   acceptMimes: ["application/json"],
   mount: {
-    "/v1": `${rootDir}/controllers/**/**Ctrl.{ts,js}`
+    "/v1": [
+      `${rootDir}/controllers/**/**Ctrl.{ts,js}`
+    ]
+  },
+  componentsScan: [
+    `${rootDir}/services/*{.ts,.js}`,
+    `${rootDir}/repositories/*{.ts,.js}`,
+    `${rootDir}/protocols/*{.ts,.js}`
+  ],
+  passport: {
+    userInfoModel: User
   },
   typeorm: [
     {
       name: "default",
       type: "postgres",
-      host: "localhost",
+      host: process.env.POSTGRES_HOST || "localhost",
       port: 5432,
-      username: "postgres",
-      password: "",
-      database: "romain.lenzotti",
-      synchronize: true,
+      username: process.env.POSTGRES_USER || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "changeme",
+      database: process.env.POSTGRES_DB || "postgres",
       logging: false,
+      synchronize: true,
       entities: [
-        `${rootDir}/entity/*{.ts,.js}`
+        `${rootDir}/entities/*{.ts,.js}`
       ],
       migrations: [
         `${rootDir}/migrations/*{.ts,.js}`
@@ -39,13 +65,21 @@ const rootDir = __dirname;
     }
   ],
   swagger: {
-    path: "/api-docs"
+    path: "/api-docs",
+    spec: {
+      securityDefinitions: {
+        "auth:basic": {
+          type: "basic"
+        }
+      }
+    }
   }
 })
 export class Server extends ServerLoader {
   $beforeRoutesInit(): void | Promise<any> {
     this
       .use(GlobalAcceptMimesMiddleware)
+      .use(cors())
       .use(cookieParser())
       .use(compress({}))
       .use(methodOverride())
@@ -57,7 +91,7 @@ export class Server extends ServerLoader {
         secret: "mysecretkey",
         resave: true,
         saveUninitialized: true,
-        maxAge: 36000,
+        // maxAge: 36000,
         cookie: {
           path: "/",
           httpOnly: true,
